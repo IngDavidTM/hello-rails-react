c3d35a5982c18559780a1d3650cc82e34aa3b18c
IngDavidTM <david-1011@hotmail.com>
2023-02-20 10:01:05 -0500

    Add react and redux functions

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App.js';
+import store from './redux/configureStore';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+);
---
+++ app/javascript/redux/greeting/greeting.js
@@ -0,0 +1,22 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const API_URL = 'http://localhost:3000/greetings/random';
+const GET_INFO = 'hello_rails_react/greetings/GET_INFO';
+const initialState = {
+    greeting: '',
+};
+
+export default ( state = initialState, action) => {
+    switch (action.type) {
+        case `${GET_INFO}/fulfilled`:
+            return { ...state, greeting: action.payload}
+         default :
+            return state;
+    }       
+};
+
+export const fetchGreeting = createAsyncThunk(GET_INFO, async () => {
+    const response = await fetch(API_URL);
+    const result = await response.json();
+    return result.message;
+});
