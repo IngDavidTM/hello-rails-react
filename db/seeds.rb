549b957a13f2a40e76235ac4688cc8103288b95c
IngDavidTM <david-1011@hotmail.com>
2023-02-21 02:16:23 -0500

    Fix rubocop errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(message: "Hello, World!")
+Greeting.create(message: "Hola, Mundo!")
+Greeting.create(message: "Salut, Monde!")
+Greeting.create(message: "Hallo, Wereld!")
+Greeting.create(message: "Hallo, Welt!")
